ff0ffa9f682c038c83e3e2e2a4e3b38bf3fde6d3
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-08-08 20:06:57 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -33,7 +33,7 @@ def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
 end
 
-def turn(board, token)
+def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
